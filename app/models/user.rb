16cd4a493c360823512fc436410fe1c6ea663912
Maikoala <maiko.s.w.0824@gmail.com>
2021-02-11 14:13:34 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday 
+  validates :first_name, :last_name, 
+            format: { with: /\A[ぁ-んァ-ン一-龥]/,
+            message: 'Full-width characters'}
+  validates :first_name_kana, :last_name_kana, 
+            format: { with: /\A([ァ-ン]|ー)+\z/,
+            message: 'Full-width katakana characters'}
+  validates :password, 
+            format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/,
+            message: 'Include both letters and numbers'}
 end
