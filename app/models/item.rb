4c87d4f150d3bb794b88af68ede8de2c211e2bd5
Maikoala <maiko.s.w.0824@gmail.com>
2021-02-17 22:10:51 +0900

    エラーメッセージ の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,20 @@
 class Item < ApplicationRecord
-  validates_presence_of :name, 
-                        :description, 
-                        :category_id, 
-                        :condition_id, 
-                        :charge_id, 
-                        :ship_from_area_id, 
-                        :delivery_time_id, 
-                        :price
-
-  validates :name, length: { maximum: 40 }
-  validates :description, length: { maximum: 1000 }
-  validates :price, numericality: { only_integer: true, 
-                                    greater_than_or_equal_to: 300,     
+  validates :image, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  with_options presence: true, numericality: { other_than: 1, message:'Select' } do
+    validates :category_id
+    validates :condition_id
+    validates :charge_id
+    validates :ship_from_area_id
+    validates :delivery_time_id
+  end
+  validates :price, presence: true,
+                    numericality: { with: /\A[0-9]+\z/, 
+                                    message: 'Half-width number' }
+  validates :price, numericality: { greater_than_or_equal_to: 300,     
                                     less_than_or_equal_to: 9999999, 
-                                    message: 'Price Out of setting range' },
-                    format: { with: /\A[0-9]+\z/ }
+                                    message: 'Out of setting range' }
 
   belongs_to :user
   has_one_attached :image
