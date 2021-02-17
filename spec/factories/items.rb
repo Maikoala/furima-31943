dc331418269ce97b43db9304b36e87a520db7b9f
Maikoala <maiko.s.w.0824@gmail.com>
2021-02-17 22:16:03 +0900

    itemモデルのテストの実装

---
+++ spec/factories/items.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :item do
+    name              { Faker::Lorem.characters(number: 10) } 
+    description       { Faker::Lorem.sentences(number: 3) }
+    category_id       { Faker::Number.between(from: 2, to: 11) }
+    condition_id      { Faker::Number.between(from: 2, to: 7) }
+    charge_id         { Faker::Number.between(from: 2, to: 3) }
+    ship_from_area_id { Faker::Number.between(from: 2, to: 48) }
+    delivery_time_id  { Faker::Number.between(from: 2, to: 4) }
+    price             { Faker::Number.between(from: 300, to: 9999999) }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test.jpg'), filename: 'test.jpg')
+    end
+  end
+end
