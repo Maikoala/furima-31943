3f93824da69ed90a35303ed90e3c29103b1b7704
Maikoala <maiko.s.w.0824@gmail.com>
2021-02-20 23:53:48 +0900

    Rubocopによる修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -116,7 +116,7 @@ RSpec.describe Item, type: :model do
       end
 
       it '価格が9,999,999円を上回る時は保存できないこと' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price Out of setting range')
       end
