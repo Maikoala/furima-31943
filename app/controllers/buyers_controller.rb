5d28c67ad7a41b974721367ebcbcf0d1b9049b94
Maikoala <maiko.s.w.0824@gmail.com>
2021-03-07 18:02:14 +0900

    UI機能の実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,4 +1,8 @@
-class BuyersController < ApplicationController
+class BuyersController < ApplicationController 
+  before_action :authenticate_user!, only: [:index]
+  before_action :move_to_index, only: [:index, :create]
+  before_action :move_to_index_buy, only: [:index, :create]
+
   def index
     @item_buyer = ItemBuyer.new
     @item = Item.find(params[:item_id])
@@ -29,5 +33,14 @@ class BuyersController < ApplicationController
       card: buyer_params[:token],
       currency: 'jpy'  
     )
+  end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user == @item.user
+  end
+
+  def move_to_index_buy
+    redirect_to root_path if user_signed_in? && @item.buyer.valid?
   end
 end
--- app/models/item_buyer.rb
+++ app/models/item_buyer.rb
@@ -3,6 +3,7 @@ class ItemBuyer
   attr_accessor :zip_code, :ship_from_area_id, :city, :address_line_1, :address_line_2, :phone_number, :user_id, :item_id, :token
   
   with_options presence: true do
+    validates :token
     validates :zip_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
     validates :ship_from_area_id, numericality: { other_than: 1, message: 'Select' }
     validates :city
@@ -10,7 +11,6 @@ class ItemBuyer
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'Input only number' }
     validates :user_id
     validates :item_id
-    validates :token
   end
 
   def save
