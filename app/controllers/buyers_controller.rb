056f025dc8737dc88b179e1a841350ec9753e8d2
Maikoala <maiko.s.w.0824@gmail.com>
2021-03-07 13:33:35 +0900

    クレジットカード決済機能の実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -8,6 +8,7 @@ class BuyersController < ApplicationController
     @item_buyer = ItemBuyer.new(buyer_params)
     @item = Item.find(params[:item_id])
     if @item_buyer.valid?
+      pay_item
       @item_buyer.save
       return redirect_to root_path
     else
@@ -18,7 +19,15 @@ class BuyersController < ApplicationController
   private
 
   def buyer_params
-    params.require(:item_buyer).permit(:zip_code, :ship_from_area_id, :city, :address_line_1, :address_line_2, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:item_buyer).permit(:zip_code, :ship_from_area_id, :city, :address_line_1, :address_line_2, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+    Payjp::Charge.create(
+      amount: @item[:price],
+      card: buyer_params[:token],
+      currency: 'jpy'  
+    )
+  end
 end
--- app/models/item_buyer.rb
+++ app/models/item_buyer.rb
@@ -1,13 +1,16 @@
 class ItemBuyer
   include ActiveModel::Model
-  attr_accessor :zip_code, :ship_from_area_id, :city, :address_line_1, :address_line_2, :phone_number, :user_id, :item_id
+  attr_accessor :zip_code, :ship_from_area_id, :city, :address_line_1, :address_line_2, :phone_number, :user_id, :item_id, :token
   
   with_options presence: true do
     validates :zip_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
     validates :ship_from_area_id, numericality: { other_than: 1, message: 'Select' }
     validates :city
     validates :address_line_1
-    validates :phone_number, format: { with: /\A[0-9]+\z/, message: 'Input only number' }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'Input only number' }
+    validates :user_id
+    validates :item_id
+    validates :token
   end
 
   def save
